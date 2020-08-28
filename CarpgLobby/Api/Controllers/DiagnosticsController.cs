c66ca128d6c70c62a44a5a959b6ddaf6d2481695
Tomashu <tomash4my@gmail.com>
2020-08-28 20:25:01 +0200

    diagnostics controller for down monitor

---
+++ CarpgLobby/Api/Controllers/DiagnosticsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace CarpgLobby.Api.Controllers
+{
+    public class DiagnosticsController : BaseController
+    {
+        [Route("api/diagnostics")]
+        public IHttpActionResult GetIsAlive()
+        {
+            return Ok();
+        }
+    }
+}
